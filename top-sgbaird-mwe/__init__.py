d1ceecf25e41a19f84dd422e86d4a86b90991e5d
sgbaird <sterling.baird@icloud.com>
2021-10-20 23:13:56 -0600

    change module folder name
    going to litter a bit on PyPI via flit because grayskull doesn't deal with TestPyPI just yet :(

---
+++ top-sgbaird-mwe/__init__.py
@@ -0,0 +1,2 @@
+"""Minimal working example for conda-smithy submodule error https://github.com/conda-forge/conda-smithy/issues/1516."""
+__version__ = "v1.0.0"
--- top/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Minimal working example for conda-smithy submodule error."""
-__version__ = "v1.0.0"
